5dfc774273c9edfdcfad76b665e30df68517af42
Anthony Fojas <anthonyjfojas@gmail.com>
2019-10-16 13:11:59 -0700

    Complete two sum

--- Two-Sum/two-sum.js
+++ Two-Sum/two-sum.js
@@ -12,5 +12,15 @@ return [0, 1].
 */
 
 var twoSum = function(nums, target) {
-    
+  // Iterate through the nums array to find and compare pairs to the target
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i+1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [nums.indexOf(nums[i]), nums.lastIndexOf(nums[j])];
+      }
+    }
+  }
+  return null;
 };
+
+// This is O(1) space and O(n^2) time
