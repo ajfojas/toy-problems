07249becc6b6d413469d6fb40613246f905683c5
Anthony Fojas <anthonyjfojas@gmail.com>
2019-10-20 23:55:41 -0700

    Complete longest common prefix

--- Longest-Common-Prefix/longest-common-prefix.js
+++ Longest-Common-Prefix/longest-common-prefix.js
@@ -18,8 +18,30 @@ All given inputs are in lowercase letters a-z.
 */
 
 var longestCommonPrefix = function(strs) {
-    
+  // If input array is empty, return ""
+  // Create a storage string
+  // Iterate through the length of the 1st string (length doesn't matter because the common prefix can only be as long as the shortest string)
+  // For each string, check if the 1st char is the same as the 1st string's 1st char
+  // If all strings share the common char, add the char to the storage string
+  // Repeat until there is no common char
+  // Return storage string
+
+  if (strs.length === 0) {
+    return '';
+  }
+
+  let commonPrefix = '';
+  for (let i = 0; i < strs[0].length; i++) {
+    for (let j = 0; j < strs.length; j++) {
+      if (strs[0][i] !== strs[j][i]) {
+        return commonPrefix;
+      }
+    }
+    commonPrefix += strs[0][i];
+  }
+
+  return commonPrefix;
 };
 
-// O() space
-// O() time
+// O(n) space
+// O(n^2) time
