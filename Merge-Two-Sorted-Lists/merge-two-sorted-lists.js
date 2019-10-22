840c8eb4fba3cfd409bd58d6fff80539fae4739d
Anthony Fojas <anthonyjfojas@gmail.com>
2019-10-21 21:53:42 -0700

    Refactor logic/variables. Add Big O

--- Merge-Two-Sorted-Lists/merge-two-sorted-lists.js
+++ Merge-Two-Sorted-Lists/merge-two-sorted-lists.js
@@ -8,8 +8,29 @@ Output: 1->1->2->3->4->4
 */
 
 var mergeTwoLists = function(l1, l2) {
-    
+
+  let head = {
+    val: null,
+    next: null
+  };
+
+  let anchor = head;
+
+  while (l1 !== null && l2 !== null) {
+    if (l1.val <= l2.val) {
+      anchor.next = l1;
+      l1 = l1.next;
+    } else {
+      anchor.next = l2;
+      l2 = l2.next;
+    }
+    anchor = anchor.next;
+  }
+
+  anchor.next = l1 === null ? l2 : l1;
+
+  return head.next;
 };
 
-// O() space
-// O() time
+// O(1) space
+// O(n + m) time
