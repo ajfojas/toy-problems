89c718b64f72bd7fd4ba0ee2c21e078b6d233af4
Anthony Fojas <anthonyjfojas@gmail.com>
2019-10-25 13:52:20 -0700

    Pseudocode

--- Sqrt(x)/sqrt(x).js
+++ Sqrt(x)/sqrt(x).js
@@ -16,7 +16,15 @@ Explanation: The square root of 8 is 2.82842..., and since the decimal part is t
 */
 
 var mySqrt = function(x) {
-    
+  // For x >= 2, the square root is always smaller than x/2 and greater than 2:   2 <= sqrt(x) <= x/2
+  // If x < 2, return x
+  // Set left bound to 2, set right bound to x/2
+  // While left <= right
+    // Take num = (left + right) / 2, compute num * num
+      // If > x, move decrement right bound by 1
+      // If < x, move increment left bound by 1
+      // If === x, return x
+  // Return right
 };
 
 // O() space
