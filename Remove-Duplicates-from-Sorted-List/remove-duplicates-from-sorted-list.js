eacc1ec104701591aed7c233ed1a383a1a3b2420
Anthony Fojas <anthonyjfojas@gmail.com>
2019-10-28 18:14:47 -0700

    Complete implementation

--- Remove-Duplicates-from-Sorted-List/remove-duplicates-from-sorted-list.js
+++ Remove-Duplicates-from-Sorted-List/remove-duplicates-from-sorted-list.js
@@ -10,9 +10,31 @@ Input: 1->1->2->3->3
 Output: 1->2->3
 */
 
+//Definition for singly-linked list.
+function ListNode(val) {
+  this.val = val;
+  this.next = null;
+}
+
 var deleteDuplicates = function(head) {
-    
+  // Keep track of current node
+  // While current node and next node !== null
+    // If next node's value === current node's value, next node === next node.next
+    // Else current node = next node
+  // Return head
+
+  let currentNode = head;
+
+  while (currentNode !== null && currentNode.next !== null) {
+    if (currentNode.next.val === currentNode.val) {
+      currentNode.next = currentNode.next.next;
+    } else {
+      currentNode = currentNode.next;
+    }
+  }
+
+  return head;
 };
 
-// O() space
-// O() time
+// O(n) space
+// O(1) time
