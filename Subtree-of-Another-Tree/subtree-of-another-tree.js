518654fba3e702352239c2ba3d17464968915c3d
Anthony Fojas <anthonyjfojas@gmail.com>
2019-10-28 20:14:04 -0700

    Complete naive solution

--- Subtree-of-Another-Tree/subtree-of-another-tree.js
+++ Subtree-of-Another-Tree/subtree-of-another-tree.js
@@ -36,8 +36,61 @@ Given tree t:
 Return false.
 */
 
+// Definition for a binary tree node
+let TreeNode = (val, left, right) => {
+  this.val = val;
+  this.left = left;
+  this.right = right;
+}
+
 var isSubtree = function(s, t) {
-    
+  // Do breadth-first search in s for a node value that equals t node value
+  // Do depth-first search in both s and t to check for equality
+  // Return true if all nodes have equal values
+  // Return false if not
+
+  let output = false;
+  
+  let queue = [];
+  queue.push(s);
+
+  let DFaccess = true;
+  let DFrecurse = (node1, node2) => {
+    if (node1 === null && node2 === null) {
+      return;
+    }
+    if (DFaccess) {
+      if (node1.val === node2.val) {
+        output = true;
+      } else {
+        output = false;
+        DFaccess = false;
+        return;
+      }
+      DFrecurse(node1.left, node2.left);
+      DFrecurse(node1.right, node2.right);
+    }
+  }
+
+  let BFrecurse = (node) => {
+    if (node.left !== null) {
+      queue.push(node.left);
+    }
+    if (node.right !== null) {
+      queue.push(node.right);
+    }
+
+    let workingNode = queue.shift();
+    if (workingNode.val === t.val) {
+      DFaccess = true;
+      DFrecurse(workingNode, t);
+    }
+    BFrecurse(queue[0])
+  };
+  
+  BFrecurse(queue[0])
+
+  return output;
 };
 
 // O() space
