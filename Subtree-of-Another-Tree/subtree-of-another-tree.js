c6176174ae512d95d93f10f93689dbd1052b2147
Anthony Fojas <anthonyjfojas@gmail.com>
2019-10-28 23:36:55 -0700

    Refactor logic/variables. Add Big O

--- Subtree-of-Another-Tree/subtree-of-another-tree.js
+++ Subtree-of-Another-Tree/subtree-of-another-tree.js
@@ -44,64 +44,23 @@ let TreeNode = (val, left, right) => {
 }
 
 var isSubtree = function(s, t) {
-  // Do breadth-first search in s for a node value that equals t node value
-  // Do depth-first search in both s and t to check for equality
-  // Return true if all nodes have equal values
-  // Return false if not
+  // Build a string of all nodes in tree s
+  // Build a string of all nodes in tree t
+  // Determine if tree t is a substring of tree s
 
-  let output = false;
-  
-  let queue = [];
-  queue.push(s);
-
-  let DFaccess = true;
-  let DFrecurse = (node1, node2) => {
-    if (node1 === null && node2 === null) {
-      return;
-    }
-    if (DFaccess) {
-      if (node1 === null || node2 === null) {
-        output = false;
-        DFaccess = false;
-        return;
-      }
-      if (node1.val === node2.val) {
-        output = true;
-      } else {
-        output = false;
-        DFaccess = false;
-        return;
-      }
-      DFrecurse(node1.left, node2.left);
-      DFrecurse(node1.right, node2.right);
-    }
-  }
-
-  let BFrecurse = (node) => {
-    if (node) {
-      if (node.left !== null) {
-        queue.push(node.left);
-      }
-      if (node.right !== null) {
-        queue.push(node.right);
-      }
-    }
-
-    let workingNode = queue.shift();
-    if (workingNode && workingNode.val === t.val) {
-      DFaccess = true;
-      DFrecurse(workingNode, t);
-    }
-
-    if (output === false) {
-      BFrecurse(queue[0]);
+  let buildStr = (node) => {
+    if (node === null) {
+      return 'null';
+    } else {
+      return '#' + node.val + ' ' + buildStr(node.left) + ' ' + buildStr(node.right);
     }
   };
-  
-  BFrecurse(queue[0]);
 
-  return output;
+  let strS = buildStr(s);
+  let strT = buildStr(t);
+
+  return strS.indexOf(strT) >= 0;
 };
 
-// O() space
-// O() time
+// O(n) space
+// O(n^2 + m^2 + n*m) time
