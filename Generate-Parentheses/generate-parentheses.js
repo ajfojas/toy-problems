8c935eb0d4bb0e7e4917d961e48d88436873ca70
Anthony Fojas <anthonyjfojas@gmail.com>
2019-11-22 17:16:07 -0800

    Pseudocode

--- Generate-Parentheses/generate-parentheses.js
+++ Generate-Parentheses/generate-parentheses.js
@@ -13,7 +13,14 @@ For example, given n = 3, a solution set is:
 */
 
 var generateParenthesis = function(n) {
-  
+  // Store all answers in a results arr
+  // Have a recursive decision tree that intakes a str and balance count
+    // Iterate through '()'
+      // If str.length === 2n && balance count === 0, push str into arr, return
+      // If str.length === 2n && balance count !== 0, return
+      // Call recursive func on (str + iteration[i], i === 0 ? count+1 : count-1)
+  // Kick off recursive func w/ ''
+  // Return results arr
 };
 
 // O() space - 
