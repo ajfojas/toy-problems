9af01fa42dd4d95925d5af8ee861f75b43d51d73
Anthony Fojas <anthonyjfojas@gmail.com>
2019-11-27 21:57:49 -0800

    Complete functionality. Add Big O

--- Search-In-Rotated-Sorted-Array/search-in-rotated-sorted-array.js
+++ Search-In-Rotated-Sorted-Array/search-in-rotated-sorted-array.js
@@ -31,7 +31,30 @@ var search = function(nums, target) {
       // If target > nums[mid] && target <= nums[right], left = mid + 1
       // Else, right = mid - 1
   // Return -1
+
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) return mid;
+    if (nums[mid] >= nums[left]) {
+      if (target >= nums[left] && target < nums[mid]) {
+        right = mid - 1;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (target > nums[mid] && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+
+  return -1;
 };
 
-// O() space - 
-// O() time - 
+// O(1) space - no additional dynamic space is used
+// O(log n) time - constantly halving the input arr
