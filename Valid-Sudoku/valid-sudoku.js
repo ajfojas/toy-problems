0734cd129f19f47839e6d34cf8b4e6a218955719
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-03 02:12:34 -0800

    Complete functionality. Add Big O

--- Valid-Sudoku/valid-sudoku.js
+++ Valid-Sudoku/valid-sudoku.js
@@ -55,14 +55,53 @@ var isValidSudoku = function(board) {
     // For each element
       // If element is ".", continue
       // If element is not in set, add it to set
-      // Else return false
+      // Else if element is already in set, return false
     // Return true
   // Take each row and check it thru the checker func
   // Take each column and check it thru the checker func
   // Take each 3x3 sub-grid and check it thru the checker func
   // If all 3 above func calls return true, return true
   // Else, return false
+
+  // Check each row's validity
+  for (let row = 0; row < board.length; row++) {
+    if (!valid(board[row])) return false;
+  }
+
+  // Check each column's validity
+  for (let row = 0; row < board.length; row++) {
+    let arr = [];
+    for (let col = 0; col < board[row].length; col++) {
+      arr.push(board[col][row]);
+    }
+    if (!valid(arr)) return false;
+  }
+
+  // Check each 3x3 sub-grid's validity
+  for (let row = 0; row < board.length; row += 3) {
+    for (let col = 0; col < board[row].length; col += 3) {
+      let row1 = [board[row][col], board[row][col + 1], board[row][col + 2]];
+      let row2 = [board[row + 1][col], board[row + 1][col + 1], board[row + 1][col + 2]];
+      let row3 = [board[row + 2][col], board[row + 2][col + 1], board[row + 2][col + 2]];
+      if (!valid(row1.concat(row2, row3))) return false;
+    }
+  }
+
+  return true;
 };
 
-// O() space - 
-// O() time - 
+let valid = (arr) => {
+  let set = new Set();
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === ".") continue;
+    if (!set.has(arr[i])) {
+      set.add(arr[i]);
+    } else if (set.has(arr[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+// O(1) space - assuming a board is always 9x9, the same amount of space will always be used
+// O(n^3) time - at most, a single check will iterate thru each element on the grid and check each arr of length 9 for validity
