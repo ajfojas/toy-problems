7e9fa5d136bfddd74a3cd099a8d5acbe8ae9862f
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-04 18:18:38 -0800

    Set up problem

---
+++ Permutations/permutations.js
@@ -0,0 +1,23 @@
+/*
+Given a collection of distinct integers, return all possible permutations.
+
+Example:
+
+Input: [1,2,3]
+Output:
+[
+  [1,2,3],
+  [1,3,2],
+  [2,1,3],
+  [2,3,1],
+  [3,1,2],
+  [3,2,1]
+]
+*/
+
+var permute = function(nums) {
+  
+};
+
+// O() space - 
+// O() time - 
