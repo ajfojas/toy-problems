68e5e687092317496b4fc6baabfb29cc973e7ea1
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-09 09:41:35 -0800

    Pseudocode

--- First-Unique-Character-In-A-String/first-unique-character-in-a-string.js
+++ First-Unique-Character-In-A-String/first-unique-character-in-a-string.js
@@ -12,7 +12,10 @@ Note: You may assume the string contain only lowercase letters.
 */
 
 var firstUniqChar = function(s) {
-
+  // Have a obj that stores char/index as key and count as value
+  // Iterate through s and store char/count in obj
+  // Iterate through s again and return index of 1st non-repeating char
+  // If it doesn't exist, return -1
 };
 
 // O() space - 
