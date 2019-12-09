dbe2ec5791c68fa80a36413956d63d716d28eba7
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-09 12:37:51 -0800

    Complete functionality. Add Big O

--- Valid-Palindrome/valid-palindrome.js
+++ Valid-Palindrome/valid-palindrome.js
@@ -22,7 +22,21 @@ var isPalindrome = function(s) {
     // Compare L and R
     // Return false if not the same
   // Return true if while-loop completes
+
+  if (s.length === 0) return true;
+
+  s = s.split(/[.,;:@#$%^&*?!'"`\(\)\/ -]/).join('').split('');
+  let L = 0;
+  let R = s.length - 1;
+
+  while (L < R) {
+    if (s[L].toLowerCase() !== s[R].toLowerCase()) return false;
+    L++;
+    R--;
+  }
+
+  return true;
 };
 
-// O() space - 
-// O() time - 
+// O(1) space - no additional dynamic space is used
+// O(n) time - at most, iterate through input 4x separately
