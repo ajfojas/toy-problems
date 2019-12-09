c12480db4568c773e6ecfbd8e19e296f40370379
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-08 21:32:35 -0800

    Pseudocode

--- Plus-One/plus-one.js
+++ Plus-One/plus-one.js
@@ -19,7 +19,22 @@ Explanation: The array represents the integer 4321.
 */
 
 var plusOne = function(digits) {
-
+  // Make a new results arr of size digits + 1
+  // Keep track of digitsIdx, starting from the end
+  // Keep track of resultsIdx, starting from end
+  // Keep track of carry over, initially set to 0
+  // Add 1 to last element of digits arr
+  // Set last element of results arr to digits[last element] % 10
+  // If digits[element] > 9
+    // Carry over = 1
+    // While there's a carry over && digitsIdx !== 0
+      // Decrement digitsIdx--
+      // Decrement resultsIdx--
+      // Add carry over to digits[element]
+      // results[element] = digits[element] % 10
+      // If digits[element] < 10, carry over = 0
+    // If digitsIdx === 0 && carry over, resultsIdx--, results[element] = 1
+  // Return results arr
 };
 
 // O() space - 
