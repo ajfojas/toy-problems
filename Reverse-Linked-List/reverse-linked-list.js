39d62f182ba404bb540c3f4cefc7037bb2d8e791
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-09 16:11:14 -0800

    Pseudocode iterative solution

--- Reverse-Linked-List/reverse-linked-list.js
+++ Reverse-Linked-List/reverse-linked-list.js
@@ -16,7 +16,16 @@ function ListNode(val) {
 }
 
 var reverseList = function(head) {
-  // 
+// Iterative:
+  // Set A ptr to null
+  // Head is already set to 1st node
+  // Set B ptr to head.next
+  // While B !== null
+    // Set head.next to A ptr
+    // Set A ptr to head
+    // Set head to B ptr
+    // Set B ptr to B.next
+  // Return head
 };
 
 // O() space - 
