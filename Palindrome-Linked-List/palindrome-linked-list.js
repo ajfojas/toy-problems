5cce32d66e4814c36a3d09f0504fd06b860448a1
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-10 16:30:13 -0800

    Complete functionality. Add Big O

--- Palindrome-Linked-List/palindrome-linked-list.js
+++ Palindrome-Linked-List/palindrome-linked-list.js
@@ -20,12 +20,51 @@ function ListNode(val) {
 }
 
 var isPalindrome = function(head) {
-  // Find the end of the 1st half
-  // Reverse the 2nd half
-  // Determine whether or not there is a palindrome
-  // Restore the list
-  // Return the result
+  // Edge case of empty input
+  if (head === null || head.next === null) return true;
+  
+  // 1. Find the end of the 1st half
+  let mid = head;
+  let fast = head;
+  while (fast.next !== null && fast.next.next !== null) {
+    mid = mid.next;
+    fast = fast.next.next;
+  }
+  
+  // 2. Reverse the 2nd half
+  reverse(mid);
+  
+  // 3. Determine whether or not there is a palindrome
+  let palindrome = true;
+  let firstHalf = head;
+  let secondHalf = mid.next;
+  while (secondHalf !== null) {
+    if (firstHalf.val !== secondHalf.val) {
+      palindrome = false;
+      break;
+    }
+    firstHalf = firstHalf.next;
+    secondHalf = secondHalf.next;
+  }
+  
+  // 4. Restore the list
+  reverse(mid);
+  
+  // 5. Return the result
+  return palindrome;
 };
 
-// O() space - 
-// O() time - 
+let reverse = mid => {
+  let prev = null;
+  let curr = mid.next;
+  while (curr !== null) {
+    let next = curr.next;
+    curr.next = prev;
+    prev = curr;
+    curr = next;
+  }
+  mid.next = prev;
+}
+
+// O(1) space - no additional dynamic space is used
+// O(n) time - at most, iterate through the LL 4x separately
