4aadbf597afe7adaa70e3909bf8722fe61d85fe8
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-11 12:21:30 -0800

    Pseudocode iterative

--- Symmetric-Tree/symmetric-tree.js
+++ Symmetric-Tree/symmetric-tree.js
@@ -29,6 +29,7 @@ function TreeNode(val) {
   this.left = this.right = null;
 }
 
+// Recursive
 var isSymmetric = function(root) {
   // Mirror image
   // Roots should have same value
@@ -41,6 +42,23 @@ let mirror = (tree1, tree2) => {
   if (tree1 === null && tree2 === null) return true;
   if (tree1 === null || tree2 === null) return false;
   return (tree1.val === tree2.val) && mirror(tree1.left, tree2.right) && mirror(tree1.right, tree2.left);
+};
+
+// Iterative
+var isSymmetric = function(root) {
+  // Have a queue
+  // Add root twice
+  // While queue is not empty
+    // tree1 = queue.shift()
+    // tree2 = queue.shift()
+    // If tree1 === null && tree2 === null, continue
+    // If tree1 === null || tree2 === null, return false
+    // If tree1.val !== tree2.val, return false
+    // Add tree1.left to queue
+    // Add tree2.right to queue
+    // Add tree1.right to queue
+    // Add tree2.left to queue
+  // Return true
 };
 
 // O(n) space - n/2 call stacks ~= n call stacks
