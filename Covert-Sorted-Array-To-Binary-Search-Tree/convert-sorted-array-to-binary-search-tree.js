193b08e71451513fa60535adc6773acb5f9734c7
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-12 12:09:46 -0800

    Result in incorrect answer. Time to refactor

--- Covert-Sorted-Array-To-Binary-Search-Tree/convert-sorted-array-to-binary-search-tree.js
+++ Covert-Sorted-Array-To-Binary-Search-Tree/convert-sorted-array-to-binary-search-tree.js
@@ -28,6 +28,31 @@ var sortedArrayToBST = function(nums) {
   // Elements to the left of mid become left childs
   // Elements to the right of mid become right childs
   // Return root
+
+  if (nums.length === 0) return;
+
+  let l = 0;
+  let r = nums.length - 1;
+  let mid = Math.floor((l + r ) / 2);
+  let root = new TreeNode(nums[mid]);
+
+  let treeTraversal = root;
+  l = mid - 1;
+  while (nums[l] !== undefined) {
+    treeTraversal.left = new TreeNode(nums[l]);
+    l--;
+    treeTraversal = treeTraversal.left;
+  }
+  
+  treeTraversal = root;
+  r = mid + 1;
+  while (nums[r] !== undefined) {
+    treeTraversal.right = new TreeNode(nums[r]);
+    r++;
+    treeTraversal = treeTraversal.right;
+  }
+
+  return root;
 };
 
 // O() space - 
