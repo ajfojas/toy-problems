d91840dc84602ed17587b21aca18d92ef1514179
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-20 16:12:11 -0800

    Complete functionality. Add Big O

--- Fizz-Buzz/fizz-buzz.js
+++ Fizz-Buzz/fizz-buzz.js
@@ -29,12 +29,27 @@ Return:
 
 var fizzBuzz = function(n) {
   // Create output array
-  // Iterate from 1 to n
+  // Iterate from 1 to n inclusive
     // For multiples of 3 && 5, push 'FizzBuzz'
     // For multiples of 3, push 'Fizz'
     // For multiples of 5, push 'Buzz'
+    // For everything else, push the current iteration value
   // Return output
+
+  let output = [];
+  for (let i = 1; i <= n; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output.push('FizzBuzz');
+    } else if (i % 3 === 0) {
+      output.push('Fizz');
+    } else if (i % 5 === 0) {
+      output.push('Buzz');
+    } else {
+      output.push(String(i));
+    }
+  }
+  return output;
 };
 
-// O() space - 
-// O() time - 
+// O(n) space - an element will be pushed into the output array
+// O(n) time - at most, iterate n times
