62f029ac6936d97e10941a5e6ea8fca7c20688c2
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-27 16:24:37 -0800

    Pseudocode

--- Count-Primes/count-primes.js
+++ Count-Primes/count-primes.js
@@ -9,7 +9,11 @@ Explanation: There are 4 prime numbers less than 10, they are 2, 3, 5, 7.
 */
 
 var countPrimes = function(n) {
-  // 
+  // Create an array of size n
+  // Iterate through array, starting at index 2, ending at sqrt(n)
+    // If array[i] !== true, iterate through all multiples of i
+      // Set array[i * j] to true
+  // Count and return all primes
 };
 
 // O() space - 
