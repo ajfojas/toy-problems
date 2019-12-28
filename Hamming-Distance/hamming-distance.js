08343b0349e493afd9b2e50ee6451bd1b1d2aa04
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-27 21:29:34 -0800

    Complete functionality. Add Big O

--- Hamming-Distance/hamming-distance.js
+++ Hamming-Distance/hamming-distance.js
@@ -23,12 +23,28 @@ The above arrows point to positions where the corresponding bits are different.
 var hammingDistance = function(x, y) {
   // Convert both inputs to binary as strings
   // Keep track of # differences
-  // Iterate through the longer string backwards
+  // Iterate through the longer string
     // If either index is null, break
     // If difference between inputs at current index, increment counter
   // Add amount of leftover digits of longer string to counter
   // Return counter
+
+  let xString = x.toString(2).split('').reverse().join('');
+  let yString = y.toString(2).split('').reverse().join('');
+  let differences = 0;
+  let longerLength
+  if (xString.length >= yString.length) {
+    longerLength = xString.length;
+  } else {
+    longerLength = yString.length;
+  }
+
+  for (let digit = 0; digit < longerLength; digit++) {
+    if (xString[digit] === '1' && yString[digit] !== '1' || yString[digit] === '1' && xString[digit] !== '1') differences++;
+  }
+
+  return differences;
 };
 
-// O() space - 
-// O() time - 
+// O(x + y) space - string conversion takes x + y space
+// O(x + y) time - converting to strings take x + y time
