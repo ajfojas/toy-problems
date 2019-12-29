8e6bb850887376fce7828dcfec65cf3c289e096b
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-29 02:04:28 -0800

    Pseudocode

--- Missing-Number/missing-number.js
+++ Missing-Number/missing-number.js
@@ -14,7 +14,10 @@ Your algorithm should run in linear runtime complexity. Could you implement it u
 */
 
 var missingNumber = function(nums) {
-  // 
+  // I can optimize either for:
+  // 1. O(n * log(n)) time and O(1) space by sorting the array first
+  // 2. O(n)          time and O(n) space by using a set and deleting entries with values I encounter
+  // I think optimizing for time would be better from a UX point of view
 };
 
 // O() space - 
