f46a1a7fd49f09c50faee12826dcb78549d17dca
Anthony Fojas <anthonyjfojas@gmail.com>
2019-12-29 20:38:18 -0800

    Complete functionality. Add Big O

--- 3Sum/3sum.js
+++ 3Sum/3sum.js
@@ -30,7 +30,36 @@ var threeSum = function(nums) {
     // Else if sum > 0, decrement 3rd index, skipping any duplicates
     // Else, increment 2nd index, skipping any duplicates
   // Return results array
+
+  let results = [];
+  nums = nums.sort((a, b) => a - b);
+
+  for (let i = 0; i < nums.length; i++) {
+    while (i > 0 && nums[i] === nums[i - 1]) i++;
+
+    let j = i + 1;
+    let k = nums.length - 1;
+
+    while (j < k) {
+      let sum = nums[i] + nums[j] + nums[k];
+      if (sum === 0) {
+        results.push([nums[i], nums[j], nums[k]]);
+        j++;
+        k--;
+        while (nums[j] === nums[j - 1]) j++;
+        while (nums[k] === nums[k + 1]) k--;
+      } else if (sum > 0) {
+        k--;
+        while (nums[k] === nums[k + 1]) k--;
+      } else {
+        j++;
+        while (nums[j] === nums[j - 1]) j++;
+      }
+    }
+  }
+
+  return results;
 };
 
-// O() space - 
-// O() time - 
+// O(n) space - results array stores triplets of elements
+// O(n^2) time - separate while-loops nested within a for-loop
